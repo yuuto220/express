b5b34801e2407239d4d3820d4b9c60fdbe39f4da
yuuto220 <yuuto15550129@icloud.com>
2026-02-02 06:19:55 +0900

    final update

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -8,14 +8,17 @@ const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
+// corsミドルウェアを使用
+router.use(cors());
+
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
 const database = client.db('notes');
 const notes = database.collection('notes');
 
-// idが１のドキュメントを取得
-const query = { id: 2 };
-const note = await notes.findOne(query);
+
+// 全てのドキュメントを取得
+const note = await notes.find({}).toArray();
 
 res.json(note);
 })
